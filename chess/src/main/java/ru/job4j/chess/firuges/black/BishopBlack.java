9fdd4dfe338510f9ae6b1489347670db30a4c4a3
NikolayKharkov <vtiko564@gmail.com>
2021-11-07 11:59:16 +0300

    2. Каркас шахматной доски [#792]

--- chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
+++ chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
@@ -27,12 +27,12 @@ public class BishopBlack implements Figure {
         Cell[] steps = new Cell[size];
         int deltaX = dest.getX() > position.getX() ? 1 : -1;
         int deltaY = dest.getY() > position.getY() ? 1 : -1;
-        int deltaXincr = deltaX > 0 ? 1 : -1;
-        int deltaYincr = deltaY > 0 ? 1 : -1;
+        int x = position.getX();
+        int y = position.getY();
         for (int index = 0; index < size; index++) {
-            steps[index] = Cell.findBy(position.getX() + deltaX, position.getY() + deltaY);
-            deltaX += deltaXincr;
-            deltaY += deltaYincr;
+            x += deltaX;
+            y += deltaY;
+            steps[index] = Cell.findBy(x, y);
         }
         return steps;
     }
--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -4,16 +4,9 @@ import org.junit.Ignore;
 import org.junit.Test;
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.black.BishopBlack;
+import ru.job4j.chess.firuges.black.KingBlack;
 
 public class LogicTest {
-
-    @Test
-    public void move()
-            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
-        Logic logic = new Logic();
-        logic.add(new BishopBlack(Cell.C1));
-        logic.move(Cell.C1, Cell.H6);
-    }
 
     @Test(expected = ImpossibleMoveException.class)
     public void whenImpossibleMove()
@@ -21,5 +14,21 @@ public class LogicTest {
         Logic logic = new Logic();
         logic.add(new BishopBlack(Cell.F7));
         logic.move(Cell.F7, Cell.B1);
+    }
+
+    @Test(expected = OccupiedCellException.class)
+    public void whenOccupiedPos()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        logic.add(new BishopBlack(Cell.A1));
+        logic.add(new BishopBlack(Cell.B2));
+        logic.move(Cell.A1, Cell.C3);
+    }
+
+    @Test(expected = FigureNotFoundException.class)
+    public void whenNotFound() throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        logic.add(new BishopBlack(Cell.F1));
+        logic.move(Cell.A1, Cell.C3);
     }
 }
